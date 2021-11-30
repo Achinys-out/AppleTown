f71070b8ce85a14296cef6f39dabe413d8b43ce7
Vladimír Ačjak <vladimiracjak@gmail.com>
2021-11-30 12:43:07 +0100

    added interaction funtionality

--- Apple Town/Assets/Scripts/Interactable.cs
+++ Apple Town/Assets/Scripts/Interactable.cs
@@ -11,18 +11,31 @@ public class Interactable : MonoBehaviour
     public KeyCode interactKey;
     public UnityEvent interactAction;
 
+    GameObject[] bubbles;
+    SpriteRenderer spBubble;
+
     private void Update()
     {
-        if (inRange && Input.GetKeyDown(interactKey)) {
+        if (inRange && Input.GetKeyDown(interactKey))
+        {
             interactAction.Invoke();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) {
+        if (collision.gameObject.CompareTag("Player"))
+        {
             inRange = true;
             Debug.Log("Player is in range of interactable object.");
+
+            bubbles = GameObject.FindGameObjectsWithTag("PopUpText");
+
+            Vector2 newPosition = this.transform.position;
+            bubbles[0].transform.position = new Vector2(newPosition.x, newPosition.y + 0.2f);
+
+            spBubble = bubbles[0].GetComponent<SpriteRenderer>();
+            spBubble.enabled = true;
         }
     }
 
@@ -32,7 +45,8 @@ public class Interactable : MonoBehaviour
         {
             inRange = false;
             Debug.Log("Player left the range of interactable object.");
+
+            spBubble.enabled = false;
         }
     }
 }
-
