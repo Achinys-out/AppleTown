99ebf6f1195051ff01962d529e5d17ccd38d4f9e
Tereza Štefíková <stefikova4@uniba.sk>
2022-01-24 22:13:48 +0100

    conversations functionality added

---
+++ Apple Town/Assets/Scripts/Dialog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class Dialog
+{
+    public string NPCName;
+    [TextArea(3,10)]
+    public string[] sentences;
+
+}
